846d5df77f2f713397e53e706c88050ce83b1fa1
WJKPK <krupskiwojciech@gmail.com>
2018-11-02 14:02:18 +0100

    Update Lista_func.c

--- Lista_func.c
+++ Lista_func.c
@@ -4,7 +4,7 @@
 
 list* init() //Funkcja inicjalizacji listy
 {
-	list* HEAD = (list*)malloc(sizeof(list));
+	list* HEAD = malloc(sizeof(HEAD));
 	HEAD->head = NULL;
 	return HEAD;
 }
@@ -13,7 +13,7 @@ int insert(list* header, int i) //Funkcja dodajaca jeden NODE zawierajacego licz
 {
 	if (header->head == NULL)
 	{
-		header->head = (node*)malloc(sizeof(node));
+		header->head = malloc(sizeof(node));
 		header->head->dane = i;
 		header->head->next = NULL;
 		return 0;
