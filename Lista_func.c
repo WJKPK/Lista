6df6595d4d46179ec55652d74246c7a0b95068f6
Wojciech Krupski <43456158+WJKPK@users.noreply.github.com>
2018-11-09 10:48:08 +0100

    Delete a comments

--- Lista_func.c
+++ Lista_func.c
@@ -9,7 +9,7 @@ listS* init() //Funkcja inicjalizacji listSy
 	return start;
 }
 
-int insert(listS* header, int i) //Funkcja dodajaca jeden NODE zawierajacego liczbe naturalna oraz adres nastepnej komorki pamieci
+int insert(listS* header, int i) 
 {
 	if (header == NULL)
 	{
@@ -38,7 +38,7 @@ int insert(listS* header, int i) //Funkcja dodajaca jeden NODE zawierajacego lic
 	return 0;
 }
 
-int pop_first(listS* header, int* c) //Funkcja usuwajaca pierwszy NODE w dostepnej liscie.
+int pop_first(listS* header, int* c) 
 {
 	if (header->head == NULL)
 	{
@@ -55,7 +55,7 @@ int pop_first(listS* header, int* c) //Funkcja usuwajaca pierwszy NODE w dostepn
 	return 0;
 }
 
-int pop_last(listS* header,int* c) //Funkcja usuwajaca ostatni NODE w dostepnej liscie. Jezeli nie ma NODE'ow informuje ze nie ma nic do usuniecia.
+int pop_last(listS* header,int* c)
 {
 	if (header->head == NULL)
 	{
@@ -92,7 +92,7 @@ int pop_last(listS* header,int* c) //Funkcja usuwajaca ostatni NODE w dostepnej 
 	return 0;
 }
 
-int clear(listS* header) //Funkcja usuwajaca cala liste
+int clear(listS* header)
 {
 	if (header == NULL)
 	{
@@ -110,7 +110,7 @@ int clear(listS* header) //Funkcja usuwajaca cala liste
 	return 0;
 }
 
-int print(const listS* header) //Funkcja printujaca cala liste
+int print(const listS* header)
 {
 	if (header->head == NULL)
 		{
